f42613d0f3dae6c246126ace7b1c8fedcdc869c6
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2024-01-03 21:54:16 +0700

    news updated

--- parkiran_test.go
+++ parkiran_test.go
@@ -3,48 +3,71 @@ package backparkir
 import (
 	"fmt"
 	"testing"
+
+	"github.com/aiteung/atdb"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 
 func TestParkiran(t *testing.T) {
-	mconn, err := SetConnection("MONGOSTRING", "PakArbi")
-	if err != nil {
-		t.Fatalf("Error connecting to MongoDB: %v", err)
-	}
-
-	var parkirandata = Parkiran{
-		ParkiranId:     1,
-		Nama:           "Muhammad Faisal Ashshidiq",
-		NPM:            "1214041",
-		Jurusan:        "D4 Teknik Informatika",
-		NamaKendaraan:  "Mio Z",
-		NomorKendaraan: "D 3316 GXF",
-		JenisKendaraan: "Motor",
-	}
-
-	result, err := CreateNewParkiran(mconn, "Parkiran", parkirandata)
-	if err != nil {
-		t.Fatalf("Error creating parkiran: %v", err)
-	}
-
-	fmt.Printf("InsertedID: %v\n", result.InsertedID)
+	mconn := SetConnection("MONGOSTRING", "PakArbi")
+	var parkirandata Parkiran
+	parkirandata.Parkiranid = "1"
+	parkirandata.Nama = "Farhan Rizki Maulana"
+	parkirandata.NPM = "1214020"
+	parkirandata.Prodi = "D4 Teknik Informatika"
+	parkirandata.NamaKendaraan = "Supra X 125"
+	parkirandata.NomorKendaraan = "F 1234 NR"
+	parkirandata.JenisKendaraan = "Motor"
+	CreateNewParkiran(mconn, "parkiran", parkirandata)
 }
 
 func TestAllParkiran(t *testing.T) {
-	mconn, err := SetConnection("MONGOSTRING", "PakArbi")
-	if err != nil {
-		t.Fatalf("Error connecting to MongoDB: %v", err)
-	}
-
-	parkiran, err := GetAllParkiran(mconn, "Parkiran")
-	if err != nil {
-		t.Fatalf("Error fetching all parkiran: %v", err)
-	}
-
-	for _, p := range parkiran {
-		fmt.Println(p)
-	}
+	mconn := SetConnection("MONGOSTRING", "PakArbi")
+	parkiran := GetAllParkiran(mconn, "parkiran")
+	fmt.Println(parkiran)
 }
+
+// func TestParkiran(t *testing.T) {
+// 	mconn, err := SetConnection("MONGOSTRING", "PakArbi")
+// 	if err != nil {
+// 		t.Fatalf("Error connecting to MongoDB: %v", err)
+// 	}
+
+// 	var parkirandata = Parkiran{
+// 		ParkiranId:     1,
+// 		Nama:           "Muhammad Faisal Ashshidiq",
+// 		NPM:            "1214041",
+// 		Jurusan:        "D4 Teknik Informatika",
+// 		NamaKendaraan:  "Mio Z",
+// 		NomorKendaraan: "D 3316 GXF",
+// 		JenisKendaraan: "Motor",
+// 	}
+
+// 	result, err := CreateNewParkiran(mconn, "Parkiran", parkirandata)
+// 	if err != nil {
+// 		t.Fatalf("Error creating parkiran: %v", err)
+// 	}
+
+// 	fmt.Printf("InsertedID: %v\n", result.InsertedID)
+// }
+
+// func TestAllParkiran(t *testing.T) {
+// 	mconn, err := SetConnection("MONGOSTRING", "PakArbi")
+// 	if err != nil {
+// 		t.Fatalf("Error connecting to MongoDB: %v", err)
+// 	}
+
+// 	parkiran, err := GetAllParkiran(mconn, "Parkiran")
+// 	if err != nil {
+// 		t.Fatalf("Error fetching all parkiran: %v", err)
+// 	}
+
+// 	for _, p := range parkiran {
+// 		fmt.Println(p)
+// 	}
+// }
 
 
 // func TestGenerateQRCode(t *testing.T) {
